fcd7aef56a2f8c9bb70303d388da0928a0b8b5ac
blu700 <jejohnso700@gmail.com>
2023-03-07 16:03:47 -0600

    slight change to spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { TableOverviewExample } from './app.component';
+import { MaterialTable } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,25 +9,25 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        TableOverviewExample
+        MaterialTable
       ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(TableOverviewExample);
+    const fixture = TestBed.createComponent(MaterialTable);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'api-call-angular'`, () => {
-    const fixture = TestBed.createComponent(TableOverviewExample);
+    const fixture = TestBed.createComponent(MaterialTable);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('api-call-angular');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(TableOverviewExample);
+    const fixture = TestBed.createComponent(MaterialTable);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('api-call-angular app is running!');
